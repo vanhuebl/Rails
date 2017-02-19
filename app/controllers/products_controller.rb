568f791e2e648e35cd12e42c3714b7b4cfc6867d
vanhuebl <vanhuebl@gmail.com>
2017-02-19 20:28:54 +0700

    add new

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,5 +70,5 @@ class ProductsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
-    end
+    end    
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
-
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
@@ -8,4 +7,17 @@ class Product < ApplicationRecord
 		%r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 		}
+
+	has_many :line_intems
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	private
+		# ensure that there are no line items referencing this product
+		def ensure_not_referenced_by_any_line_item
+			unless line_intems.empty?
+				errors.add(:base, 'Line Items present')
+				throw :abort
+			end
+			
+		end
 end
