f266cd2e4a3d5361bd163a8222721f39d6c97586
vanhuebl <vanhuebl@gmail.com>
2017-03-06 21:30:29 +0700

    check out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
   #get 'store/index'
   root 'store#index', as: 'store_index'
 
-  resources :products
+  resources :products do
+  	get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
